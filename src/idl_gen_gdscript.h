412793cebe5180e996812c9321bfffe8af496e31
Samuel Nicholas <nicholas.samuel@gmail.com>
2024-05-04 22:20:32 +0930

    duplicated the cpp code gen files for gdscript
    stripped them of everything except some exprimentation

---
+++ src/idl_gen_gdscript.h
@@ -0,0 +1,13 @@
+#ifndef FLATBUFFERS_IDL_GEN_GDSCRIPT_H_
+#define FLATBUFFERS_IDL_GEN_GDSCRIPT_H_
+
+#include "flatbuffers/code_generator.h"
+
+namespace flatbuffers {
+
+// Constructs a new GDScript code generator.
+std::unique_ptr<CodeGenerator> NewGDScriptCodeGenerator();
+
+}  // namespace flatbuffers
+
+#endif  // FLATBUFFERS_IDL_GEN_GDSCRIPT_H_
